26cba6536e796ae4bef9a440bc74a5edf15a529f
Kristjan Retter <krretter@gmail.com>
2019-06-21 22:57:45 +0300

    api done

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import "./App.css";
 import VenueMap from "./components/VenueMap/VenueMap";
 import { threadId } from "worker_threads";
+import Map from "./components/VenueMap/VenueMap";
 
+declare interface Math {
+  radians(): any;
+}
 export interface AppProps {}
 
 export interface AppState {
@@ -36,6 +40,10 @@ class App extends React.Component<AppProps, AppState> {
 
   componentDidMount() {
     fetch(
+      "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Ossu%20Burger&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyCiqvVWt3WbgB72f4Nx30eL-2Ls0J9oxvM"
+    ).then(data => console.log(data));
+
+    fetch(
       "https://api.foursquare.com/v2/venues/search/?categoryId=4bf58dd8d48988d16c941735&near=Tartu&client_id=BFUGT4CB2RB2FATARCVQBYCD1VM0YHXNOY14L03SW21LMURD&client_secret=AL2DICAO3E1BNPG22VHL5PBFUWWL4DWNOUXRSTISRL3UHYWK&v=20131124"
     )
       .then(data => data.json())
@@ -47,15 +55,21 @@ class App extends React.Component<AppProps, AppState> {
       });
 
     /*fetch(
-      "https://api.foursquare.com/v2/venues/4f1be43de4b07beffe6f983a/photos/?client_id=BFUGT4CB2RB2FATARCVQBYCD1VM0YHXNOY14L03SW21LMURD&client_secret=AL2DICAO3E1BNPG22VHL5PBFUWWL4DWNOUXRSTISRL3UHYWK&v=20131124"
+      "https://api.foursquare.com/v2/venues/541ce992498e9ab3265ed894/photos/?limit=10&client_id=BFUGT4CB2RB2FATARCVQBYCD1VM0YHXNOY14L03SW21LMURD&client_secret=AL2DICAO3E1BNPG22VHL5PBFUWWL4DWNOUXRSTISRL3UHYWK&v=20131124"
     )
       .then(data => data.json())
       .then(data => {
         console.log(data);
-        this.setState({
-          prefix: data.response.photos.items[0].prefix,
-          suffix: data.response.photos.items[0].suffix
-        });
+
+        if (
+          data.response.photos.items[0].prefix !== undefined &&
+          data.response.photos.items[0].suffix !== undefined
+        ) {
+          this.setState({
+            prefix: data.response.photos.items[0].prefix,
+            suffix: data.response.photos.items[0].suffix
+          });
+        }
       });*/
   }
 
@@ -68,14 +82,31 @@ class App extends React.Component<AppProps, AppState> {
   };
 
   render() {
-    console.log();
     // const pilt = this.state.books.response.photos.items[0].prefix;
+
     const url = `${this.state.prefix}300x500${this.state.suffix}`;
     return (
       <div className="App">
-        <div style={{ height: "400px", width: "100%" }} className="map-wrapper">
-          <VenueMap venues={this.state.venues} />
-        </div>
+        <img src={url} />
+        <section
+          style={{ width: "100%", height: `38rem` }}
+          className="map-wrap"
+        >
+          <div style={{ height: "39vh", width: "100%;" }} className="map">
+            <Map
+              center={{ lat: 58.38545402237506, lng: 26.359866734165085 }}
+              zoom={13}
+              places={this.state.venues}
+              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCiqvVWt3WbgB72f4Nx30eL-2Ls0J9oxvM"
+              loadingElement={<div style={{ width: "100%", height: `100%` }} />}
+              containerElement={
+                <div style={{ width: "100%", height: `38rem` }} />
+              }
+              mapElement={<div style={{ width: "100%", height: `100%` }} />}
+            />
+            ,
+          </div>
+        </section>
         {this.handleVenues()}
       </div>
     );
--- src/components/VenueMap/VenueMap.tsx
+++ src/components/VenueMap/VenueMap.tsx
@@ -1,67 +1,81 @@
-import React, { Component } from "react";
+/* global google */
+import React, { Fragment } from "react";
 import burger from "../../assets/logos/burger.svg";
-import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
-import "./VenueMap.scss";
+import {
+  withGoogleMap,
+  GoogleMap,
+  withScriptjs,
+  Marker,
+  Circle
+} from "react-google-maps";
 
 export interface VenueMapProps {
-  google: any;
-  venues: any;
+  places: any;
+  zoom: any;
+  center: any;
 }
 
 export interface VenueMapState {}
 
 class VenueMap extends React.Component<VenueMapProps, VenueMapState> {
-  state = {
-    lat: 58.38275705619358,
-    lng: 26.74649799484187,
-    zoom: 14,
-    showingInfoWindow: false,
-    activeMarker: {},
-    selectedPlace: {}
+  state = { bussStation: { lat: 58.37832, lng: 26.73246 } };
+  distance = (lat1: number, lon1: number, lat2: number, lon2: number) => {
+    const R = 6371; // Radius of the earth in km
+    const dLat = ((lat2 - lat1) * Math.PI) / 180; // deg2rad below
+    const dLon = ((lon2 - lon1) * Math.PI) / 180;
+    const a =
+      0.5 -
+      Math.cos(dLat) / 2 +
+      (Math.cos((lat1 * Math.PI) / 180) *
+        Math.cos((lat2 * Math.PI) / 180) *
+        (1 - Math.cos(dLon))) /
+        2;
+
+    return R * 2 * Math.asin(Math.sqrt(a));
   };
-  componentDidMount() {
-    return;
-  }
-  handleClick = () => {
-    return this.setState({
-      lat: this.props.venues[3].location.lat,
-      lng: this.props.venues[3].location.lng,
-      zoom: 14
+
+  handleVenues = () => {
+    return this.props.places.map((place: any) => {
+      const BussStationDistance = this.distance(
+        this.state.bussStation.lat,
+        this.state.bussStation.lng,
+        place.location.lat,
+        place.location.lng
+      );
+      if (BussStationDistance > 1) {
+        return (
+          <Marker
+            icon={{ url: burger, scaledSize: new google.maps.Size(30, 30) }}
+            position={{
+              lat: place.location.lat,
+              lng: place.location.lng
+            }}
+          />
+        );
+      }
     });
   };
 
-  onMarkerClick = (props: any, marker: any, e: any) => {
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
-  };
   render() {
-    console.log(this.state.lat);
     return (
-      <div>
-        <Map
-          google={this.props.google}
-          zoom={this.state.zoom}
-          initialCenter={{
-            lat: this.state.lat,
-            lng: this.state.lng
+      <GoogleMap
+        defaultZoom={this.props.zoom}
+        defaultCenter={{
+          lat: 58.38012,
+          lng: 26.72245
+        }}
+      >
+        <Circle
+          defaultCenter={{
+            lat: this.state.bussStation.lat,
+            lng: this.state.bussStation.lng
           }}
-        >
-          <Marker
-            icon={burger}
-            onClick={this.onMarkerClick}
-            title={"Changing Colors Garage"}
-            position={{ lat: this.state.lat, lng: this.state.lng }}
-          />
-        </Map>
-        <button onClick={this.handleClick}>Change</button>
-      </div>
+          radius={1000}
+        />
+        {this.handleVenues()}
+      </GoogleMap>
     );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyAECk90BXS-7Qrpb3vUJAs1tKW3gDSdlOE"
-})(VenueMap);
+export default withScriptjs(withGoogleMap(VenueMap));
